0a6a46f1962c315336fb628bbe96dda2404d2176
hikarabiruyou <rihidelicous@gmail.com>
2018-01-20 21:12:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req,res) => {
     const now = new Date().getTime();
-    res.setHeadr('Set-Cookie','last_access='+now+';');
+    res.setHeader('Set-Cookie',['last_access=' + now + ';','test=testdata;']);
     res.end('hi');
 });
 const port = 8000;
