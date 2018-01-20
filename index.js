c7810430e421be05e628e37cc3ddd5550232cb6f
hikarabiruyou <rihidelicous@gmail.com>
2018-01-21 00:09:00 +0900

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req,res) => {
     const now = new Date().getTime();
     res.setHeader('Set-Cookie',['last_access=' + now + ';','test=testdata;']);
-    res.end('hi');
+    const last_access_time = req.headers.coolie ?  parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+    res.end(new Date(last_access_time).toString());
 });
 const port = 8000;
 server.listen(port,() => {
